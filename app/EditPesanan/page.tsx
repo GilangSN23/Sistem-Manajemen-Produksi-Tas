0bfe7668794cdf7b716c704b8d18b02aacb5df1a
GilangSN23 <119424777+GilangSN23@users.noreply.github.com>
2025-06-19 13:38:29 +0700

    fixxx

--- app/EditPesanan/page.tsx
+++ app/EditPesanan/page.tsx
@@ -1,19 +1,10 @@
-import { type FC } from "react";
 import EditPesanan from "@/components/EditPesanan";
 
-interface PageProps {
-  params: {
-    orderId: string;
-  };
-}
-
-const EditPesananPage: FC<PageProps> = ({ params }) => {
+export default function EditPesananPage({ params }: { params: { orderId: string } }) {
   return (
     <div className="py-8">
       <h1 className="text-2xl font-bold mb-4 text-center">Edit Pesanan</h1>
       <EditPesanan orderId={params.orderId} />
     </div>
   );
-};
-
-export default EditPesananPage;
+}
